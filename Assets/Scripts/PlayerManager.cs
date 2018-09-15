50cd371c7da5540984aebff24b6a683ea63ccf93
Sokolov O <trainerxs@gmail.com>
2018-09-15 23:15:54 +0300

    AddForce update for player

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce(0,0, 2000 * Time.deltaTime);
+		
+	}
+}
